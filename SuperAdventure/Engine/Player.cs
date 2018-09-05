387134346707a89e3aa3409f321a93e1a7e70542
Danilo <wendigoster@gmail.com>
2018-09-04 23:17:31 -0300

    añadiendo la carpeta de archivos

---
+++ SuperAdventure/Engine/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine
+{
+    public class Player
+    {
+        public int CurrentHitPoints { get; set; }
+        public int MaximumhitPoints { get; set; }
+        public int Gold { get; set; }
+        public int ExperiencePoints { get; set; }
+        public int Level { get; set; }
+    }
+}
